5b40ec069064cdf7e8ee6528270f0b10df58271f
Code Help Tutor <1823890830@qq.com>
2024-04-06 06:16:46 +0000

    WeChat cstutorcs cs348 2022 spring hw4 Rvvse master

---
+++ test/example2.c
@@ -0,0 +1,17 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+#include "homework.h"
+
+int main() {
+  int x = source();
+  int y;
+  if (x > 0) {
+    y = sanitizer(x);
+    sink(y); // safe
+  } else {
+    y = x;
+  }
+  sink(y); // bug
+  return 0;
+}
---
+++ test/homework.h
@@ -0,0 +1,6 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+extern int source();
+extern void sink(int);
+extern int sanitizer(int);
